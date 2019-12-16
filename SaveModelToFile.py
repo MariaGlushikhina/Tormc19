87a9264f91340fbf7045d9b66182d22a4c44a0c1
MariaGlushikhina <56550907+MariaGlushikhina@users.noreply.github.com>
2019-12-16 15:53:12 +0300

    Update SaveModelToFile.py

--- SaveModelToFile.py
+++ SaveModelToFile.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import pickle
+import struct
 def SaveModelToFile(dat, fileToWrite, code = 'zeus'):
    
     if code=='zeus':
@@ -29,105 +31,11 @@ def SaveModelToFile(dat, fileToWrite, code = 'zeus'):
                     strToWrite = str(dat.dd[i,k])+ "\n"+"\n"
                     f.write(strToWrite)       
 
-        # print("writing Vr_cyl velocity [cm s^-1]")
-        # f.write("\n")
-        # f.write("Vr_cyl velocity [cm s^-1]\n")
-        # for k in range(0,dat.nz-1):
-        #     for j in range(0,dat.nt-1):
-        #         for i in range(0,dat.nr-1):
-        #             strToWrite = str(dat.Usc*Mx[i,j,k])+ "\n"
-        #             f.write(strToWrite)   
-
-        # print("writing Vt velocity [cm s^-1]")
-        # f.write("\n")
-        # f.write("Vt_cyl velocity [cm s^-1]\n")
-        # for k in range(0,dat.nz-1):
-        #     for j in range(0,dat.nt-1):
-        #         for i in range(0,dat.nr-1):
-        #             strToWrite = str(dat.Usc*Mt[i,j,k])+ "\n"
-        #             f.write(strToWrite)    
-
-        # print("writing Vz velocity [cm s^-1]")
-        # f.write("\n")
-        # f.write("Vr_cyl velocity [cm s^-1]\n")
-        # for k in range(0,dat.nz-1):
-        #     for j in range(0,dat.nt-1):
-        #         for i in range(0,dat.nr-1):
-        #             # strToWrite = str(i)+str(j)+str(k)+' '+str(dat.Usc*Mz[i,j,k])+ "\n"
-        #             strToWrite = str(dat.Usc*Mz[i,j,k])+ "\n"
-        #             f.write(strToWrite)    
-
-
-        # print("SaveModelToFile ... done")
-    
+       
     
     f.close()
 
 
-
-#  ====================================================================
-
-    #  f = open(fileToWrite, 'w')
-
-    #     f.write("Fortran-style indexing: F(i,j,k) \n:\
-    #     do z=>k =slowest, phi=>j, x=>i=fastest \n\n " )
-
-    #     strToWrite ="nr:" +str(dat.nr-1)+' nph:'+str(dat.nt-1)+' nz:'+str(dat.nz-1)+"\n"
-    #     f.write(strToWrite)
-
-    #     print("writing x-coord")
-    #     f.write("i-coord: cyl. radius [cm]\n")
-    #     for i in range(0,dat.nr):
-    #         f.write(str(dat.Rsc*dat.r[i,0,0])+"\n")       
-
-    #     print("writing phi-coord")
-    #     f.write("\n")
-    #     f.write("j-coord: theta [rad] \n")
-    #     for j in range(0,dat.nt):
-    #         f.write(str(dat.t[0,j,0])+"\n")       
-
-    #     print("writing z-coord, [cm]")
-    #     f.write("\n")
-    #     f.write("k-coord: z \n")
-    #     for k in range(0,dat.nz):
-    #         f.write(str(dat.Rsc*dat.z[0,0,k])+"\n")       
-
-    #     print("writing number density [cm^-3]")
-    #     f.write("\n")
-    #     f.write("number density [cm^-3] \n")
-    #     for k in range(0,dat.nz-1):
-    #         for j in range(0,dat.nt-1):
-    #             for i in range(0,dat.nr-1):
-    #                 strToWrite = str(dat.n0*dat.d[i,j,k])+ "\n"
-    #                 f.write(strToWrite)       
-
-    #     print("writing Vr_cyl velocity [cm s^-1]")
-    #     f.write("\n")
-    #     f.write("Vr_cyl velocity [cm s^-1]\n")
-    #     for k in range(0,dat.nz-1):
-    #         for j in range(0,dat.nt-1):
-    #             for i in range(0,dat.nr-1):
-    #                 strToWrite = str(dat.Usc*Mx[i,j,k])+ "\n"
-    #                 f.write(strToWrite)   
-
-    #     print("writing Vt velocity [cm s^-1]")
-    #     f.write("\n")
-    #     f.write("Vt_cyl velocity [cm s^-1]\n")
-    #     for k in range(0,dat.nz-1):
-    #         for j in range(0,dat.nt-1):
-    #             for i in range(0,dat.nr-1):
-    #                 strToWrite = str(dat.Usc*Mt[i,j,k])+ "\n"
-    #                 f.write(strToWrite)    
-
-    #     print("writing Vz velocity [cm s^-1]")
-    #     f.write("\n")
-    #     f.write("Vr_cyl velocity [cm s^-1]\n")
-    #     for k in range(0,dat.nz-1):
-    #         for j in range(0,dat.nt-1):
-    #             for i in range(0,dat.nr-1):
-    #                 # strToWrite = str(i)+str(j)+str(k)+' '+str(dat.Usc*Mz[i,j,k])+ "\n"
-    #                 strToWrite = str(dat.Usc*Mz[i,j,k])+ "\n"
-    #                 f.write(strToWrite)    
 
 
 def ReadModelFromFile(dat, file, code = 'zeus'):
@@ -153,7 +61,7 @@ def ConvertAnsSaveCylindricalToCart(dat, x,y,z, fileToWrite):
     
     f = open(fileToWrite, 'w')
     
-    strToWrite = str(dat.Nx-1)+ " "  +str(dat.Ny-1)+ " "+ str(dat.Nz-1)+ '\n'
+    strToWrite = str(dat.Nx-2)+ " "  +str(dat.Ny-2)+ " "+ str(dat.Nz-2)+ '\n'
           
     
     f.write("1 \n")  #iformat
@@ -189,11 +97,11 @@ def ConvertAnsSaveCylindricalToCart_1(dat,  DD, x,y,z,  fileToWrite):
     f.write(strToWrite+"\n") #number of cells
     f.write("1 \n")      #nrspec - number of independent dust species
 
-    for i,xi in zip(range(dat.Nx-1), x):
+    for i,xi in zip(range(dat.Nx-2), x):
    
-      for j,yj in zip(range(dat.Ny-1), y): 
+      for j,yj in zip(range(dat.Ny-2), y): 
                  
-            for k,zk in zip(range(dat.Nz-1), z):
+            for k,zk in zip(range(dat.Nz-2), z):
                
                 rc = 1.0*np.sqrt(xi**2 + yj**2)                                 
                 r_index = np.where(dat.x+2 >=rc)[0][0]            
@@ -218,24 +126,4 @@ def ConvertAnsSaveCylindricalToCart_1(dat,  DD, x,y,z,  fileToWrite):
          
 
 
-# for all x,y,z do 
 
-#     rc2 = x**2 + y**2 + z**2    
-#     rc = sqrt(rc2)
-    
-#     Rcyl = sqrt( x**2 + y**2 )    
-#     phi = atan2(y,x)
-#     phi= math.atan2(yj,xi) 
-    
-    
-#     interpolate phi -> t find j_t
-    
-#     interpolate Rcyl -> r find i_r    
-    
-#     interpolate z -> z' find k_z
-                
-# end
-
-# r = interp1d(Rcyl, r)
-
-# itemindex = numpy.where(array==item)
